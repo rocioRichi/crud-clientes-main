27491f3b66765768ebd63e0677386b887a19ed83
Rocio Ricciardiello <paragit@gmail.com>
2025-08-15 22:01:43 +0200

    Remove footer

--- resources/views/_comun/pie.blade.php
+++ resources/views/_comun/pie.blade.php
@@ -1,7 +1,7 @@
 <footer id="footer" class="bg-dark footer mt-auto py-3 text-muted">
     <div class="container">
         <div class="row d-flex align-items-center" style="height: 50px">
-            <div class="col-md-6 text-center text-md-start text-white">Ramon Abramo</div>
+
             <div class="col-md-6 text-center text-md-end text-white"> Aplicacion Web</div>
         </div>
     </div>
